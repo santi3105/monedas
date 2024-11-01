cf28eb8965370ddbf2ce7e2d06df8a58b6757094
Santiago Fandiño Nieto <fandinosantiago00@gmail.com>
2024-11-01 13:32:37 -0500

    se agrego funcion menu

---
+++ monedas.py/interfaz.py
@@ -0,0 +1,11 @@
+def menu():
+    """
+    muestra el menu de las monedas 
+    """
+    print("\n Bienvenido a la calculadora de monedas a dolares y euros")
+    print("1)Pesos colombianos")
+    print("2)Bolivares")
+    print("3)pesos mexicanos")
+    print("4)salir")
+    op = int(input("digite una opcion del menu: "))
+    return op
